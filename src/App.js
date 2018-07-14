c507c44cefc50c3523341002dc4aac6d3a3b1747
Alexandre Pelagaggi <alexandre@phygitall.rio>
2018-07-14 17:42:30 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import smile from './smile.jpg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Alexandre Pelagaggi</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to my personal webpage. Soon an amazing profile will be here.<br /><br />
+          hope to see you then.<br /><br /><img style={{ width: 10 + 'vw' }} src={smile}/>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
