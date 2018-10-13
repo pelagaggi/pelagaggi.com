e0b04fa5418696358ee46b6bed9d68ea59e638ec
Alexandre Pelagaggi <pelagaggialex@gmail.com>
2018-10-13 16:48:33 +0100

    Updated curriculum

--- src/App.js
+++ src/App.js
@@ -25,53 +25,35 @@ class App extends Component {
         <Row>
           <Profile/>
           <Col>
-            <Experience 
-              logo={phygitall} 
+            <Experience
+              logo={phygitall}
               intro={'Full stack developer intern at Phygitall (IoT solutions company) since Aug/2017'}
-              description={`Full stack developer intern at Phygitall (IoT solutions company) since 06/11/201, working in
-              SQL databases modeling as well as developing front end web apps that are deployed and
-              sustained by Node.js socket.io web sockets that work with react and bootstrap 4 as front end.
-              Having experience with LoRaServer and Lora networks utilizing Mqtt and Rest APIs. Fast
-              prototyping demands with Node-red. Experience with AWS cloud environment, RDS, EC2,
-              CloudWatch and GIT CLI software versioning. Also having aiding with Kicad PCB design
-              firmware for ESP8266 micro controllers as raspberry Pi slaves`}
+              description={`Middleware and Full stack developer intern in an IoT solutions company (Phygitall) since November 2017. Developing mainly NodeJS middleware, ReactJS components, managing PostgreSQL databases, developing Android Native Apps. Working in LPWANs aimed applications in geolocation, telemetry, indoor location as well as LoRaWAN network integration architecture in Actility and LoRa Server network Server and performing R&D in Indoor Locations models for 2.4Ghz RF using Python machine learning. Also managing in AWS IAM, EC2 and security groups management integrated with NGINX to manage web domains and subdomains.`}
             />
-            <Experience 
-              logo={TKA} 
+            <Experience
+              logo={TKA}
               intro={'Invited Java Teacher for an 21h Java SE 1.8 Intesive course. Sep/2017'}
-              description={`Java programming invited instructor on behalf of British company The Knowledge
-              Academy (September 2017). Deploying a 21h course covering Java 8 SE features such as
-              lambda expressions, multi-tasking development and JavaFX introduction`}
+              description={`Java programming invited instructor on behalf of British company The Knowledge Academy. Deploying a 21h course covering Java 8 SE features such as lambda expressions, multi-tasking development and JavaFX introduction`}
             />
-            <Experience 
+            <Experience
               logo={eletro} 
               intro={'Internship in VBA Visio solution from Nov/2016 to Nov/2017'}
-              description={`Intern in instrumentation and control section of Eletronuclear-Eletrobrás from 07/12/2016 to
-              01/12/2017, acting as a software developer for nuclear power plant Angra 2 and Angra 3,
-              assisting in control room GUI human factors engineering via VBA solutions for microsoft
-              VISIO and punctually QT software development.`}
+              description={`•	He was Intern in instrumentation and control section of Eletronuclear-Eletrobras, acting as a software developer for nuclear power plant Angra 2 and Angra 3, assisting in control room GUI human factors engineering via VBA solutions for Microsoft VISIO and punctually developing QT M2M supervisory systems.`}
             />
-            <Experience 
+            <Experience
               logo={cefet} 
               intro={'Researcher in power electronics solid state devices from Oct/2016 to Nov/2017'}
-              description={`CEFET-RJ researcher in power electronics lab LAEP since October 2016-December 2017,
-              aiming smart grid technology utilizing Arduino Mega microcontroller and adding JAVA
-              GUI to control active power electronics components and accomplish IoT applications.`}
+              description={`•	CEFET-RJ researcher in power electronics lab LAEP, aiming smart grid technology utilizing Arduino Mega micro-controller and adding JAVA GUI to control power electronics components and accomplish smart grid aimed IoT applications.`}
             />
-            <Experience 
+            <Experience
               logo={isep} 
               intro={'Erasmus student from Fev/2016 to Aug/2016'}
-              description={`Six-month Academic Exchange Program at Instituto Superior de Engenharia do Porto in
-              Portugal, during 2016 first semester, having studied distributed systems development for
-              UNIX/POSIX OS utilizing C. Multi-platform applications with Java SE 8.`}
+              description={`Six-month Academic Exchange Program at Instituto Superior de Engenharia do Porto in Portugal, during 2016 first semester, having studied distributed systems development for UNIX/POSIX OS utilizing C. Multi-platform applications with Java SE 8 and Atmel microcontrollers family`}
             />
-            <Experience 
+            <Experience
               logo={cnpq} 
               intro={'Researcher In organic solar cells aimed thin films development from Jan/2014 to Jan/2016'}
-              description={`CNPq Researcher, in the Experimental Physics Laboratory (LaFEA / CEFET-RJ), for 11
-              months, being 5 months in development of DSSCs, and 6 months in Thin Films Deposition
-              Process by chemical and physical deposition(Sputtering).Having all verbal and written
-              communication in the English, due to constant contact with foreigners, 2014-2015.`}
+              description={`CNPq Researcher, in the Experimental Physics Laboratory (LaFEA / CEFET-RJ), for 11 months, being 5 months in development of DSSCs, and 6 months in Thin Films Deposition Process by chemical and physical deposition (Sputtering). Having all verbal and written communication in the English, due to constant contact with foreigners`}
             />
           </Col>
         </Row>
